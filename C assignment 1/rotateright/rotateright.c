0392e082d4b4e488d4a832c7a24d974ac08081f4
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-07 13:17:25 -0800

    c_assignment

---
+++ C assignment 1/rotateright/rotateright.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+	int n;
+	int i = 0;
+	int p;
+	int count = 0;
+	scanf_s("%d", &n);
+	scanf_s("%d", &p);
+	i = n;
+	while (n > 0)
+	{
+		count++;
+		n = n / 10;
+	}
+	n = i;
+	//printf("%d", count);
+	printf("\n");
+	int m;
+	for (int j = 0; j < p; j++)
+	{
+		m = n % ((int)pow(10, (count - 1)));
+		// printf("%d", m);
+		m = m*(pow(10, (count - (count - 1))));
+		//printf("\n%d", m);
+		m = m + (n / pow(10, (count - 1)));
+		n = m;
+	}
+	printf("%d", n);
+
+	getch();
+	return 0;
+	scanf_s("%d", &n);
+}
