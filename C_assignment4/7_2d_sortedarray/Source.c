d739249e93d7f99a8803279dfd52611415fecdb5
pranitha <pranitha285@gmail.com>
2019-02-10 23:14:31 +0530

    c_assignment4

---
+++ C_assignment4/7_2d_sortedarray/Source.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+
+#include<ctype.h>
+
+#include<string.h>
+
+int main()
+
+{
+
+	char **s = NULL;
+
+	int n, j, i;
+
+	printf("Enter n:");
+
+	scanf_s("%d", &n);
+
+	s = (char **)malloc(n * sizeof(char *));
+
+	for (i = 0; i<n; i++)
+
+		*(s + i) = (char *)malloc(30 * sizeof(char));
+
+	if (s == NULL)
+
+		printf("memory allocation failed\n");
+
+	printf("Enter strings:");
+
+	for (i = 0; i<n; i++)
+
+		scanf_s("%s", s[i]);
+
+	char *tmp;
+
+	for (i = 0; i<n - 1; i++)
+
+	{
+
+		for (j = i; j<n; j++)
+
+		{
+
+			int d = strcmp(s[i], s[j]);
+
+			if (d>0)
+
+			{
+
+				tmp = *(s + i);
+
+				*(s + i) = *(s + j);
+
+				*(s + j) = tmp;
+
+			}
+
+		}
+
+	}
+
+	printf("After swapping:");
+
+	for (i = 0; i<n; i++)
+
+		puts(s[i]);
+
+	return 0;
+
+}
+
